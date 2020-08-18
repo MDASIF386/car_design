0de17ea01da27e19cf2ba52b7fe68a71e2550b14
asif <asifdanish7654@gmail.com>
2020-08-19 01:26:48 +0530

    car

---
+++ script.js
@@ -0,0 +1,6 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'C:\\Users\\ANPL\\Desktop\\Car Aminated\\sound.mp3');
+audio.loop=true;
+audio.play();
+// var audi=new audio('sound.mp3');
+// audio.play();
